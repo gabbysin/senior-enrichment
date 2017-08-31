850f2ae2c46504bb1d3262fce64bdcff1e7e8804
Gabrielle Sin <sin.gabrielle@gmail.com>
2017-08-31 14:45:21 -0400

    adjusted image sizes and made inline

--- app/components/AllCampuses.js
+++ app/components/AllCampuses.js
@@ -5,16 +5,27 @@ import { fetchCampuses, changeCampus } from '../reducers';
 
 function AllCampuses(props){
   const { allCampuses } = props;
+
+  const imgStyle = {
+    width: "128px",
+  }
+
+  const formStyle = {
+    display: "inline-block",
+    padding: "30px"
+  }
+
   return (
   <div >
     {
       allCampuses.map(campus => (
-        <div  key={campus.id}>
+        <div  key={campus.id} style={formStyle}>
           <NavLink 
+            className="main-all-campuses"
             to={`/campus/${campus.id}`}
             onClick={props.handleSelectCampus}
             >
-            <img src={ campus.img } />
+            <img src={ campus.img }    style={imgStyle}/>
             <div >
               <h5>
                 <span>{ campus.name }</span>
